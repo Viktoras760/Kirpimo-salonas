7e50db4a3716b50be06a35486d6d66aeb8122452
Viktoras760 <35394736+Viktoras760@users.noreply.github.com>
2022-12-08 15:58:18 +0200

    Reservation list filtered by tags added
    0.5h

--- reservation.php
+++ reservation.php
@@ -4,6 +4,9 @@ include_once "auth_session.php";
 require_once "config.php";
 $barber;
 $result = mysqli_query($mysqli,"SELECT * FROM services");
+$result2 = mysqli_query($mysqli,"SELECT * FROM services");
+$type = $_POST['id'];;
+$services = mysqli_query($mysqli,"SELECT * FROM services WHERE Tags = '$type'");
 ?>
 
 <!DOCTYPE html>
@@ -25,57 +28,77 @@ $result = mysqli_query($mysqli,"SELECT * FROM services");
                                 <?php if(!empty($_SESSION['error']) ) { ?>
                                 <div class="alert alert-danger"><?php echo $_SESSION['error']; ?></div>
                                 <?php } $_SESSION['error'] = NULL ?>
-                                <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <?php if($_SESSION["role"] == 'Barber' || $_SESSION["role"] == 'Admin'){ ?> 
-                                    <a href="add_service.php"><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Pridėti naują paslaugą"></input></a><?php } ?>
-                                </div>
-                                <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                    <?php if($_SESSION["role"] != NULL){ ?> 
-                                    <a href="reservation.php"><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 12px;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: -apple-system,system-ui,Roboto,Helvetica,Arial,sans-serif;font-size: 16px;font-weight: 500;line-height: 2.5;outline: transparent;padding: 0 1rem;text-align: center;text-decoration: none;transition: box-shadow .2s ease-in-out;user-select: none;-webkit-user-select: none;touch-action: manipulation;white-space: nowrap;" value="Registruotis"></input></a><?php } ?>
-                                </div>
+                                <div class="col-md-6 col-lg-3 d-none d-md-block">
+                                    <table style="border-spacing: 1; border-collapse: collapse; background:white;border-radius:6px;overflow:hidden; width:100%;margin:0 auto;position:relative;">
+                                        <thead>
+                                            <tr style="height:60px;background:#f33333;font-size:16px;">
+                                                <th>Pasirinkite norimos paslaugos tipą</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            
+                                            <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
+                                            <td><form method="POST" action="reservation.php"><input type="hidden" name="id" value='<?php echo "Moteriškas kirpimas";?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Moteriškas kirpimas"></form></td>
+                                            </tr> 
 
-                                <table style="border-spacing: 1; border-collapse: collapse; background:white;border-radius:6px;overflow:hidden; width:100%;margin:0 auto;position:relative;">
-                                    <thead>
-                                        <tr style="height:60px;background:#FFED86;font-size:16px;">
-                                            <th>Paslauga</th>
-                                            <th>Paslaugos kainas (Eur)</th>
-                                            <th>Trukmė (min)</th>
-                                            <th>Kirpėjas</th>
-                                            <th></th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php while($row = mysqli_fetch_array($result)){ ?>
-                                    <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
-                  	                    <td title="Paslauga"> <?php echo $row['Name'] ?> </td>
-                                        <td title="Paslaugos kainas (Eur)"> <?php echo $row['Price'] ?> </td>
-                                        <td title="Trukmė (min)"> <?php echo $row['Duration'] ?> </td>
-                                        <?php $sql = "SELECT * FROM users WHERE Personal_code = ?";
-      
-                                        if($stmt = $mysqli->prepare($sql))
-                                        {
-                                            // Bind variables to the prepared statement as parameters
-                                            $stmt->bind_param("s", $row['fk_Barber_code']);
+                                            <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
+                                            <td><form method="POST" action="reservation.php"><input type="hidden" name="id" value='<?php echo "Vyriškas kirpimas";?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Vyriškas kirpimas"></form></td>
+                                            </tr> 
 
-                                            // Attempt to execute the prepared statement
-                                            if($stmt->execute())
+                                            <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
+                                            <td><form method="POST" action="reservation.php"><input type="hidden" name="id" value='<?php echo "Plaukų dažymas (vyrams)";?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Moteriškas plaukų dažymas"></form></td>
+                                            </tr> 
+
+                                            <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
+                                            <td><form method="POST" action="reservation.php"><input type="hidden" name="id" value='<?php echo "Plaukų dažymas (moterims)";?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Vyriškas plaukų dažymas"></form></td>
+                                            </tr> 
+                                    
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="col-md-6 col-lg-9 d-none d-md-block">
+
+                                    <table style="border-spacing: 1; border-collapse: collapse; background:white;border-radius:6px;overflow:hidden; width:100%;margin:0 auto;position:relative;">
+                                        <thead>
+                                            <tr style="height:60px;background:#FFED86;font-size:16px;">
+                                                <th>Paslauga</th>
+                                                <th>Paslaugos kainas (Eur)</th>
+                                                <th>Trukmė (min)</th>
+                                                <th>Kirpėjas</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php while($row = mysqli_fetch_array($services)){ ?>
+                                        <tr style="height:48px; border-bottom:1px solid #E3F1D5 ;">
+                                            <td title="Paslauga"> <?php echo $row['Name'] ?> </td>
+                                            <td title="Paslaugos kainas (Eur)"> <?php echo $row['Price'] ?> </td>
+                                            <td title="Trukmė (min)"> <?php echo $row['Duration'] ?> </td>
+                                            <?php $sql = "SELECT * FROM users WHERE Personal_code = ?";
+        
+                                            if($stmt = $mysqli->prepare($sql))
                                             {
-                                                $res = $stmt->get_result();
-                                                $row2 = $res->fetch_assoc();
-                                                $barber = $row2['Name'];
-                                            } 
-                                           
-                                            // Close statement
-                                            $stmt->close();
-                                        } ?>
-                                        <td title="Kirpėjas"> <?php echo $barber ?> </td>
-                                        <td><form method="POST" action="edit_auth_service.php"><input type="hidden" name="id" value='<?php echo $row['id_Services'];?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Paslaugos aprašymas"></form></td>
-                                        <td><form method="POST" action="delete_service.php"><input type="hidden" name="id" value='<?php echo $row['id_Services'];?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Ištrinti paslaugą"></form></td>
-                                    </tr> 
-                                    <?php } ?> 
-                                    </tbody>
-                                </table>
+                                                // Bind variables to the prepared statement as parameters
+                                                $stmt->bind_param("s", $row['fk_Barber_code']);
+
+                                                // Attempt to execute the prepared statement
+                                                if($stmt->execute())
+                                                {
+                                                    $res = $stmt->get_result();
+                                                    $row2 = $res->fetch_assoc();
+                                                    $barber = $row2['Name'];
+                                                } 
+                                            
+                                                // Close statement
+                                                $stmt->close();
+                                            } ?>
+                                            <td title="Kirpėjas"> <?php echo $barber ?> </td>
+                                            <td><form method="POST" action="time_reservation.php"><input type="hidden" name="id" value='<?php echo $row['id_Services'];?>'><input type="submit" style="background: linear-gradient(to bottom right, #EF4765, #FF9A5A);border-radius: 8px;border-style: none;box-sizing: border-box;color: #FFFFFF;cursor: pointer;display: inline-block;font-family: Helvetica, Arial, sans-serif;font-size: 14px;font-weight: 500;height: 40px;line-height: 20px;list-style: none;margin: 0;outline: none;padding: 10px 16px;position: relative;text-align: center;text-decoration: none;transition: color 100ms;vertical-align: baseline;user-select: none;-webkit-user-select: none;touch-action: manipulation;" value="Rezervuoti laiką"></form></td>
+                                        </tr> 
+                                        <?php } ?> 
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
